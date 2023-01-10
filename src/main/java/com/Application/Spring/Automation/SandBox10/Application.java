9e6243a89602b467d018ef244515735b113577e8
tomeryoel <tomerys@gmail.com>
2023-01-10 12:11:54 +0200

    Initial commit

---
+++ src/main/java/com/Application/Spring/Automation/SandBox10/Application.java
@@ -0,0 +1,14 @@
+package com.Application.Spring.Automation.SandBox10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("ma nisgar im ze kvar?!?!?!?!?!?");
+		SpringApplication.run(Application.class, args);
+	}
+
+}
